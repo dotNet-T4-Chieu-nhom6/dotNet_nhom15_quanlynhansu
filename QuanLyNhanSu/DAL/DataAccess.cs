befa92ff23a0334a79848a86541ae4cf54eeab52
quanglopxe <quanglopxe@gmail.com>
2023-12-17 23:02:11 +0700

    referenced 3 layers model and fixed code

---
+++ QuanLyNhanSu/DAL/DataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class ConnectionData
+    {
+        public static SqlConnection Connect()
+        {
+            string conStr = "Data Source=DESKTOP-US92MIO\QUANGLOPXE;Initial Catalog=QuanLyNhanSu;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(conStr);
+            return conn;
+        }
+    }
+    public class DataAccess
+    {
+        
+    }
+}
--- QuanLyNhanSu/DTO/Employee.cs
+++ QuanLyNhanSu/DTO/Employee.cs
@@ -8,121 +8,84 @@ namespace DTO
 {
     public class Employee
     {
-        private string name;
-        private DateTime birthdate;
-        private string address;
+        private int idnv;
+
+        public int Idnv
+        {
+            get { return idnv; }
+            set { idnv = value; }
+        }
+        private string tennv;
+
+        public string Tennv
+        {
+            get { return tennv; }
+            set { tennv = value; }
+        }
+        private DateTime ngaysinh;
+
+        public DateTime Ngaysinh
+        {
+            get { return ngaysinh; }
+            set { ngaysinh = value; }
+        }
+        private string gioitinh;
+
+        public string Gioitinh
+        {
+            get { return gioitinh; }
+            set { gioitinh = value; }
+        }
+        private string diachi;
+
+        public string Diachi
+        {
+            get { return diachi; }
+            set { diachi = value; }
+        }
         private string email;
-        private string gender;
-        private int phoneNumber;
-        private string education;
-        private string expertise;
-        private int id;
-        private string position;
-        private double salary;
-        private string department;
-        private string status;
-        private double benefit;
-
-        public double Benefit
-        {
-            get { return benefit; }
-            set { benefit = value; }
-        }
-
-
-        public string Status
-        {
-            get { return status; }
-            set { status = value; }
-        }
-
-        public string Department
-        {
-            get { return department; }
-            set { department = value; }
-        }
-
-        public double Salary
-        {
-            get { return salary; }
-            set { salary = value; }
-        }
-
-        public string Position
-        {
-            get { return position; }
-            set { position = value; }
-        }
-
-        public int Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
-
-        public string Education
-        {
-            get { return education; }
-            set { education = value; }
-        }
-
-        public string Expertise
-        {
-            get { return expertise; }
-            set { expertise = value; }
-        }
-
-        public int PhoneNumber
-        {
-            get { return phoneNumber; }
-            set { phoneNumber = value; }
-        }
-
-        public string Gender
-        {
-            get { return gender; }
-            set { gender = value; }
-        }
 
         public string Email
         {
             get { return email; }
             set { email = value; }
         }
+        private string didong;
 
-        public string Address
+        public string Didong
         {
-            get { return address; }
-            set { address = value; }
+            get { return didong; }
+            set { didong = value; }
+        }
+        private string vanhoa;
+
+        public string Vanhoa
+        {
+            get { return vanhoa; }
+            set { vanhoa = value; }
+        }
+        private int chuyenmon;
+
+        public int Chuyenmon
+        {
+            get { return chuyenmon; }
+            set { chuyenmon = value; }
+        }
+        private int idphongban;
+
+        public int Idphongban
+        {
+            get { return idphongban; }
+            set { idphongban = value; }
+        }
+        private int luong;
+
+        public int Luong
+        {
+            get { return luong; }
+            set { luong = value; }
         }
 
-        public DateTime Birthdate
-        {
-            get { return birthdate; }
-            set { birthdate = value; }
-        }
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        public Employee(string name, DateTime birthdate, string address, string email, string gender, int phoneNumber, string education, string expertise, int id, string position, double salary, string department, string status, double benefit)
-        {
-            this.name = name;
-            this.birthdate = birthdate;
-            this.address = address;
-            this.email = email;
-            this.gender = gender;
-            this.phoneNumber = phoneNumber;
-            this.education = education;
-            this.expertise = expertise;
-            this.id = id;
-            this.position = position;
-            this.salary = salary;
-            this.department = department;
-            this.status = status;
-            this.benefit = benefit;
-        }
+        
     }
 }
